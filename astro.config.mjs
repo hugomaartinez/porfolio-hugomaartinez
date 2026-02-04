e536e639c105e49cc8f9b92e1eb7a0cbbc316e40
Hugo Martínez Barros <hugo.martinez.barros@digitalizat.com>
2026-02-04 22:08:25 +0100

    chore: setup SEO with sitemap, robots.txt and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
+import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+  site: 'https://hugomaartinez.dev'
 })
