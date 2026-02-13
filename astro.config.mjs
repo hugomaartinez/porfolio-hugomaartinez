b4bed7be194744b7d4159a06db5f1061ba096cc5
Hugo Martínez Barros <hugo.martinez.barros@digitalizat.com>
2026-02-13 23:57:19 +0100

    Fix: Cloudflare deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: 'https://hugomaartinez.dev',
-  output: 'static',
-  adapter: cloudflare()
+  output: 'static'
 })
