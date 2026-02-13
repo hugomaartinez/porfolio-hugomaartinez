2cb52d299503289aee6c8b20f8f8cc5ca5a3ff06
Hugo Martínez Barros <hugo.martinez.barros@digitalizat.com>
2026-02-13 23:39:20 +0100

    feat: Add Cloudflare adapter to Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
-  site: 'https://hugomaartinez.dev'
+  site: 'https://hugomaartinez.dev',
+  adapter: cloudflare()
 })
